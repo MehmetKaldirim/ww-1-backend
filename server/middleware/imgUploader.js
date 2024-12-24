a263e6d9eecb731c498b4fc765d66d356e31ac97
Mehmet <mathdenizi@gmail.com>
2024-12-24 04:56:14 +0100

    adding relative path for server deploying

--- server/middleware/imgUploader.js
+++ server/middleware/imgUploader.js
@@ -1,32 +1,31 @@
-const multer = require('multer');
-const moment = require('moment');
-const path = require('path');
-
+const multer = require("multer");
+const moment = require("moment");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, './server/uploads/');
-    },
-    filename(req, file, cb) {
-        const date =  moment().format('DDDMMYYYY-HHmmss_SSS');
-        cb(null, `${date}-${file.originalname}`);
-    }
+  destination(req, file, cb) {
+    cb(null, process.env.PWD + "./uploads/");
+  },
+  filename(req, file, cb) {
+    const date = moment().format("DDDMMYYYY-HHmmss_SSS");
+    cb(null, `${date}-${file.originalname}`);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
-        cb(null,true);
-    } else {
-        cb(null,false);
-    }
+  if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
 const limits = {
-    fileSize: 1024 * 1024 * 5,
-}
+  fileSize: 1024 * 1024 * 5,
+};
 
 module.exports = multer({
-    storage,
-    fileFilter,
-    limits
+  storage,
+  fileFilter,
+  limits,
 });
